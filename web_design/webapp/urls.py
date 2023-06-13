18cd3a2baa9c3c49866594d53283bd4c22d03cf9
21sudheer <sudheervarmap1999@gmail.com>
2023-06-13 17:33:58 +0530

    responsive

---
+++ web_design/webapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("chat_view_function/", views.chat_view_function, name="chat_view_function"),
+    path("", views.chat1, name="chat1"),
+    path("search/", views.search, name="search"),
+    path("updates", views.updates, name="updates"),
+    path("faq/", views.faq, name="faq"),
+    path("upgrade/", views.upgrade, name="upgrade"),
+    path("upfad/", views.upfad, name="upfad"),
+    path("demo/", views.demo, name="demo"),
+    path("new/", views.new, name="new"),
+    path("subscription/", views.subscription, name="subscription"),
+    path("photo_retouching/", views.photo_retouching, name="photo_retouching"),
+    path("talking_avatar/", views.talking_avatar, name="talking_avatar"),
+]
---
+++ web_design/webapp/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def chat_view_function(request):
+    return render(request, "dashboard.html")
+
+
+def chat1(request):
+    return render(request, "chat1.html")
+
+
+def search(request):
+    return render(request, "search.html")
+
+
+def updates(request):
+    return render(request, "updates&FAQ.html")
+
+
+def faq(request):
+    return render(request, "FAQ.html")
+
+
+def upgrade(request):
+    return render(request, "upgrade.html")
+
+
+def upfad(request):
+    return render(request, "updfaq.html")
+
+
+def demo(request):
+    return render(request, "toggle.html")
+
+
+def new(request):
+    return render(request, "new.html")
+
+
+def subscription(request):
+    return render(request, "sub.html")
+
+
+def photo_retouching(request):
+    return render(request, "photo_retouching.html")
+
+
+def talking_avatar(request):
+    return render(request, "talking_avatar.html")
